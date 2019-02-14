a99d02ab4a0df9fc0bc2d85a56044dae75848776
Joseph <joseph@stechstudio.com>
2019-02-14 15:05:20 -0500

    script to setup the symlink

--- extension.php
+++ extension.php
@@ -8,3 +8,5 @@ if (file_exists(__DIR__.'/vendor/autoload.php')) {
 use Illuminate\Container\Container;
 
 Container::getInstance()->singleton('Valet\PhpFpm', ValetPhpBrew\PhpFpm::class);
+
+$app->setName($app->getName() . " with PhpBrew");
